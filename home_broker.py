b715e367a95df1f51fb2728d2f64547cb18e9172
Caio Victor <caiovictor.pcb@gmail.com>
2024-11-28 19:23:23 -0300

    init

---
+++ home_broker.py
@@ -0,0 +1,111 @@
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+import json
+import requests
+from datetime import datetime, timedelta
+import os
+load_dotenv()
+import re
+
+
+prompt = """
+Você recebe a seguinte mensagem de investimento:
+
+{message}
+
+Extraia as seguintes informações e as insira no formato JSON e retorne apenas os valores:
+- Direção da aposta, se for de venda o valor deve ser "down", se for de compra deve ser "up" (nome do campo: direcao)
+- Duração da aposta em milissegundos (exemplo: 60000) (nome do campo: duracao_aposta_ms)
+- Símbolo do ativo da aposta (exemplo: "EUR") (nome do campo: simbolo_ativo)
+- É criptomoeda? (exemplo: True) (nome do campo: is_cripto)
+- Horário de início da entrada (exemplo: 16:15) (nome do campo: horario_inicio_aposta)
+"""
+
+template = PromptTemplate.from_template(prompt)
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2
+)
+
+chain = template | llm
+
+credentials = {
+    "token": None,
+    "expiration_date": None
+}
+
+home_broker_credentials = {
+    "username": os.environ.get("HOME_BROKER_EMAIL"),
+    "password": os.environ.get("HOME_BROKER_PASSWORD"),
+    "role": "hbb"
+}
+
+bet_value_usd_cents = 100
+account_type = "demo"
+
+def format_response(response):
+    raw_content = response.json()
+    json_data = raw_content.strip("```json").strip("```").strip()
+    extracted_data = json.loads(json_data)
+    result = json.dumps(extracted_data, indent=2)
+
+    result = json.loads(result)
+
+    content_raw = result["content"]
+
+    cleaned_content = content_raw.replace("```json", "").replace("```", "").strip()
+
+    data = json.loads(cleaned_content)
+    return data
+
+
+def get_home_broker_payload_from_fenri_message(telegram_message, isOtc):
+    response = chain.invoke({"message": telegram_message})
+    raw_payload = format_response(response)
+    isCripto = raw_payload["is_cripto"]
+    horario_aposta = raw_payload["horario_inicio_aposta"]
+    today = datetime.now()
+    time_today = datetime.strptime(horario_aposta, "%H:%M").replace(year=today.year, month=today.month, day=today.day)
+    utc_time = time_today + timedelta(hours=3)
+    iso_format = utc_time.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+    ticker_symbol = raw_payload["simbolo_ativo"] + ('-USD' if isCripto else "") + ('-OTC' if isOtc else '')
+    ticker_symbol = re.sub(r'(?<=-USD)-USD', '', ticker_symbol, count=1)
+    home_broker_payload = {
+        "direction": raw_payload["direcao"],
+        "bet_value_usd_cents": bet_value_usd_cents,
+        "duration_milliseconds": raw_payload["duracao_aposta_ms"],
+        "ticker_symbol": ticker_symbol,
+        "account_type": account_type,
+        "client_start_price": 1,
+        "start_time_utc": iso_format
+    }
+    return home_broker_payload, horario_aposta
+
+
+def update_credentials():
+    res = requests.post("https://account-manager-api.homebroker.com/login", data=json.dumps(home_broker_credentials))
+    data = res.json()
+    if "accessToken" not in data:
+        print("Erro ao atualizar credenciais.")
+        print(data)
+        return
+    credentials["token"] = data["accessToken"]
+    credentials["expiration_date"] = datetime.now() + timedelta(seconds=450)
+    print("Credenciais atualizadas com sucesso!")
+
+
+def make_fenri_trade(payload: dict):
+    if credentials["token"] is None or datetime.now() > credentials["expiration_date"]:
+        update_credentials()
+    res = requests.post("https://trade-api-edge.homebroker.com/op/",data=json.dumps(payload), headers= {
+        "Authorization": f"Bearer {credentials['token']}"
+    })
+    data = res.json()
+    if "id" in data:
+        return data["id"], data
+    return None, data
---
+++ main.py
@@ -0,0 +1,56 @@
+from telethon import TelegramClient, events
+from home_broker import make_fenri_trade, get_home_broker_payload_from_fenri_message, update_credentials
+from datetime import datetime, timedelta
+import threading
+import os
+
+API_ID = os.environ.get("TELEGRAM_API_ID")
+API_HASH = os.environ.get("TELEGRAM_API_HASH")
+
+client = TelegramClient('session_name', API_ID, API_HASH)
+
+
+def make_trade(payload: dict):
+    trade_id, data = make_fenri_trade(payload)
+    if trade_id is not None:
+        print(f"Trade criado com ID: {trade_id}")
+    else:
+        print(f"Erro ao criar trade: {payload['ticker_symbol']}; {payload['direction']}; {payload['start_time_utc']}; {payload['duration_milliseconds']}")
+        print(f"error: {data}")
+
+
+def schedule_function(run_time: str, func, *args, **kwargs):
+
+    now = datetime.now()
+    try:
+        target_time = datetime.strptime(run_time, "%H:%M").replace(year=now.year, month=now.month, day=now.day) - timedelta(seconds=10)
+
+        delay = (target_time - now).total_seconds()
+        threading.Timer(delay, func, args, kwargs).start()
+        print(f"Function scheduled to run at: {target_time}")
+    except ValueError:
+        print("Invalid time format. Please ensure the time is in HH:mm format.")
+
+async def main():
+    await client.start()
+    print("Conectado ao Telegram!")
+    update_credentials()
+
+    chats = [-1002350028496, -1002453860229, -1002453860229]
+
+    @client.on(events.NewMessage(chats=chats))
+    async def handler(event):
+        """Callback para novas mensagens."""
+        message = event.message.message
+        if "Investimento Identificado" in message or "Trade confirmado" in message:
+            print("Mensagem de investimento identificada!")
+            isOtc = "(OTC)" in message
+            payload, horario_aposta = get_home_broker_payload_from_fenri_message(message, isOtc)
+            print(f"{payload['ticker_symbol']}; {payload['direction']}; {payload['start_time_utc']}; {payload['duration_milliseconds']}")
+            schedule_function(horario_aposta, make_trade, payload)
+
+    print("Escutando novas mensagens...")
+    await client.run_until_disconnected()
+
+with client:
+    client.loop.run_until_complete(main())
